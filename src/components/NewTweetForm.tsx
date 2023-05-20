7749836683c2647cf9dc9114d24dad57710b2604
Emre Cakiroglu <yunusemre34tr@gmail.com>
2023-05-20 12:47:50 +0300

    fix indentation

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,21 +1,46 @@
 import { useSession } from "next-auth/react";
 import { Button } from "./Button";
 import { ProfileImage } from "./ProfileImage";
+import { useCallback, useLayoutEffect, useRef, useState } from "react";
+
+function updateInputTextSize(textArea?: HTMLTextAreaElement) {
+  if (!textArea) return;
+  textArea.style.height = "0px";
+  textArea.style.height = `${textArea.scrollHeight}px`;
+}
 
 export default function NewTweetForm(){
-    const session = useSession()
+  const session = useSession();
+  if (session.status !== "authenticated") return null;
 
-    if (session.status !== "authenticated") return;
+  return <Form/>;
+}
 
-    return (
-        <form className="flex flex-col gap-2 border-b px-4 py-2">
-            <div className="flex gap-4">
-                <ProfileImage src={session.data.user.image} />
-                <textarea className="flex-grow resize-none overflow-hidden p4 text-lg outline-none"
-                placeholder="What's happening?"/>
+function Form(){
+  const session = useSession();
+  const [inputText, setInputText] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updateInputTextSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
 
-            </div>
-            <Button className="self-end">Tweet</Button>
-        </form>
-    )
+  useLayoutEffect(() => {
+    updateInputTextSize(textAreaRef.current);
+  }, [inputText]);
+
+  return (
+    <form className="flex flex-col gap-2 border-b px-4 py-2">
+      <div className="flex gap-4">
+        <ProfileImage src={session.data !== null ? session.data.user.image : null} />
+        <textarea
+          ref={inputRef}
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)} 
+          className="flex-grow resize-none overflow-hidden p4 text-lg outline-none"
+          placeholder="What's happening?"/>
+      </div>
+      <Button className="self-end">Tweet</Button>
+    </form>
+  )
 }
