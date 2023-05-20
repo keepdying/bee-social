83c6da36fc5d5eef7a01ca47420773ab31b0ac28
Emre Cakiroglu <yunusemre34tr@gmail.com>
2023-05-20 12:04:03 +0300

    implemented abstract page layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { SideNav } from "~/components/SideNav";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +14,20 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>Bee Social</title>
+        <meta 
+          name="description" 
+          content="Social network app, only for ITU students."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto flex items-start sm:pr-4">
+        <SideNav />
+        <div className="min-h-screen flex-grow border-x">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </SessionProvider>
   );
 };
